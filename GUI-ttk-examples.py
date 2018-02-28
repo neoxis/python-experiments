211eea61324aa307fa4d6861a370a3a4e2df70f2
Brad Stiff <bstiff@iastate.edu>
2018-02-28 12:00:28 -0600

    added scrolled text and messagebox

--- GUI-ttk-examples.py
+++ GUI-ttk-examples.py
@@ -1,5 +1,15 @@
 from tkinter import *
-from tkinter.ttk import *
+from tkinter.ttk import *               #combobox library
+from tkinter import scrolledtext        #scolled text library
+from tkinter import messagebox          #message box library
+
+#button functions#
+def clicked():
+        txt.delete(1.0, END)
+
+def mes():
+        messagebox.showinfo('Message Title','Message Body')
+
 
 ##########
 ###MAIN###
@@ -7,3 +17,39 @@ window =Tk()
 window.title("GUI-ttk Examples")
 window.geometry()
 window.mainloop
+
+##Button##
+##########
+''' Clear '''
+b = Button(window, text="Clear", command=clicked)
+b.grid(column=1, row=0)
+''' Message Box '''
+b1 = Button(window, text="Message", command=mes)
+b1.grid(column=1, row=1)
+
+##Combobox##
+############
+c = Combobox(window)
+c['values']=(1,2,3,4,5)
+c.current(2)
+c.grid(column=0, row=0)
+
+##Checkbutton##
+###############
+
+
+
+
+
+##Radiobutton##
+###############
+
+
+
+##Scrolledtext##
+################
+'''Example 1 - Scrolled Text ''' #width/height = number of chars
+txt = scrolledtext.ScrolledText(window, width=50, height=10)
+txt.insert(INSERT, 'Your text goes here...')
+
+txt.grid(column=0, row=1)
