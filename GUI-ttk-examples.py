77c68041f659f5d6530764011f736e3a0ae64f50
Brad Stiff <bstiff@iastate.edu>
2018-02-28 14:19:21 -0600

    added messagebox examples

--- GUI-ttk-examples.py
+++ GUI-ttk-examples.py
@@ -8,7 +8,22 @@ def clicked():
         txt.delete(1.0, END)
 
 def mes():
-        messagebox.showinfo('Message Title','Message Body')
+        '''Example 1 - show info '''
+        #messagebox.showinfo('Message Title','Message Body')
+        '''Example 2 - show warning '''
+        #messagebox.showwarning('Message Title','Message Body')
+        '''Example 3 - show error '''
+        #messagebox.showerror('Message Title','Message Body')
+        '''Example 4 - ask question '''
+        #messagebox.askquestion('Message Title','Message Body')
+        '''Example 5 - ask yes or no '''
+        #messagebox.askyesno('Message Title','Message Body')
+        '''Example 6 - ask yes, no, or cancel '''
+        #messagebox.askyesnocancel('Message Title','Message Body')
+        '''Example 7 - ask ok or cancel '''
+        #messagebox.askokcancel('Message Title','Message Body')
+        '''Example 8 - ask retry or cancel '''
+        messagebox.askretrycancel('Message Title','Message Body')
 
 
 ##########
@@ -25,13 +40,14 @@ b = Button(window, text="Clear", command=clicked)
 b.grid(column=1, row=0)
 ''' Message Box '''
 b1 = Button(window, text="Message", command=mes)
-b1.grid(column=1, row=1)
+b1.grid(column=2, row=0)
 
 ##Combobox##
 ############
 c = Combobox(window)
 c['values']=(1,2,3,4,5)
 c.current(2)
+
 c.grid(column=0, row=0)
 
 ##Checkbutton##
@@ -52,4 +68,4 @@ c.grid(column=0, row=0)
 txt = scrolledtext.ScrolledText(window, width=50, height=10)
 txt.insert(INSERT, 'Your text goes here...')
 
-txt.grid(column=0, row=1)
+txt.grid(column=0, row=1, columnspan=3)
